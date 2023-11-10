5ab7a896129edd120c0fc4269202d1ef1087a790
Daniel Lee <daniel@dyl-macbook.local>
2023-11-09 21:27:03 -0800

    Disable inmemory ISR.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  experimental: {
+    isrMemoryCacheSize: 0, // disable default in-memory caching
+  },
 };
 
 module.exports = nextConfig;
