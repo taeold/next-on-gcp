90dea55ad3183031e6ea245a40d907a744595dd3
Daniel Lee <daniel@dyl-macbook.lan>
2023-11-15 08:21:47 -0800

    BUG: Can't get nested node from RTDB.

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   output: "standalone",
   experimental: {
     isrMemoryCacheSize: 0, // disable default in-memory caching
+    incrementalCacheHandlerPath: "../lib/rtdb-cache-2.js",
   },
   generateBuildId: async () => {
     return process.env.BUILD_ID || "local-build";
